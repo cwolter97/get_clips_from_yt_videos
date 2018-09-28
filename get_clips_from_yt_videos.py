3950f0aa20d2e16286ed1ddb4b9468c882864962
Cody Wolter <cwolter97@gmail.com>
2018-09-28 01:09:23 -0500

    Misc Updates to Script
    Added Comments, TODO Items, and reversed video quality list.

--- get_clips_from_yt_videos.py
+++ get_clips_from_yt_videos.py
@@ -17,6 +17,7 @@ def extract_chunk(video_path,start,end,targetname):
 	mr = start%60
 	s = int(end/60)
 	sr = end%60
+	# Shows timestamp of chunk being extracted
 	print("Extracting {:02d}:{:02d} - {:02d}:{:02d} from {}".format(m,mr,s,sr,video_path.split('/')[-1]))
 	path = settings['output_directory']
 	ffmpeg_extract_subclip(video_path, start, end, targetname=path+targetname)
@@ -27,22 +28,26 @@ def get_chunks(stream,file_handle):
 	ignore_start_seconds = settings['ignore_start_seconds']
 	ignore_end_seconds = settings['ignore_end_seconds']
 
+	# gets name from open file handle (everything after the '/' and before the last '.' in full filepath)
 	name = ''.join(file_handle.name.split('/')[-1].split('.')[:-1])
 
 	for i in range(chunk_quantity):
 		print("\n\nGetting Chunk {}".format(i+1))
+								# bounds the max int so we won't ever get a clip that goes into our ignored setting
 		start_rand = random.randint(ignore_start_seconds,(int(stream.length)-(ignore_end_seconds+chunk_seconds)))
 		end_rand = start_rand + chunk_seconds
 
 		extract_chunk(file_handle.name,start_rand,end_rand,name+" - CHUNK {}.mp4".format(i+1))
 
+	# Cleanup, remove original file
 	os.remove(file_handle.name)
 
 def download_video(video_url,quality,download_path):
-	# TODO: Complete quality_order list. Make hybrid that will download higher quality video then combine audio
-	# to accomplish higher quality downloads. 
+	# TODO:
+	# Complete quality_order list.
+	# Make hybrid that will download higher quality video then combine audio to accomplish higher quality chunks. 
 
-	quality_order = ["1080p","720p","480p","360p","240p","144p"]
+	quality_order = ['144p', '240p', '360p', '480p', '720p', '1080p']
 
 	quality_index = quality_order.index(quality)
 
@@ -52,15 +57,16 @@ def download_video(video_url,quality,download_path):
 	
 	# Decide which to download
 	found_quality = False
-	while not found_quality and quality_index < len(quality_order):
+	while not found_quality and quality_index >= 0:
 		for v in yt:
 			if quality == v.resolution and v.includes_audio_track:
 				found_quality = True
 				yt = v
 				break
 		if not found_quality:
+			# Decrement to next quality in list
 			old_quality = quality
-			quality_index += 1		
+			quality_index -= 1		
 			quality = quality_order[quality_index]
 			print("Video + Audio not available in {}. Looking in {} now.".format(old_quality,quality))
 
@@ -74,7 +80,9 @@ def download_video(video_url,quality,download_path):
 	return yt_obj
 
 def main():
-	# TODO: Add threading to speed up when downloading multiple videos
+	# TODO:
+	# Add threading to speed up when downloading multiple videos
+	# Add validation on config
 
 	video_list = settings['videos']
 	quantity = settings['quantity']
@@ -99,6 +107,7 @@ def main():
 		print("\nSleeping for {} minutes...".format(frequency/60))
 		sleep(frequency)
 
+#Load settings
 with open('config.json') as config_file:
 	settings = json.load(config_file)
 
@@ -107,6 +116,7 @@ if 'videos' not in settings:
 		settings['videos'] = video_file.readlines()
 
 if __name__ == "__main__":
+	# TODO: Allow command-line arguments to set config file and/or video list file
 	main()
 
 __author__ = "Cody Wolter"
